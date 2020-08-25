13cc936474e15cdf24fd75c4781d9109c59f9657
Seonghyeon Kim <impressor7@gmail.com>
2020-08-26 01:40:45 +0900

    Update not to use yarn workspaces

--- packages/create-apollo-server/lib/create-apollo-server.ts
+++ packages/create-apollo-server/lib/create-apollo-server.ts
@@ -13,7 +13,7 @@ import { generateApolloConfig, generateAppConfig } from './utils';
 
 let projectDir: string | undefined;
 const TEMPLATE = {
-  version: '0.0.1',
+  version: '0.0.2',
   name: '@seonghyeonkimm/cas-template',
 };
 
